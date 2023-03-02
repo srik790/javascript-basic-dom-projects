dd54b965cd979efc913c4a23c118e487d8570a33
Shrikant Kumar <srik790@gmail.com>
2023-03-02 21:28:24 +0530

    set to setup v1

--- 01-color-flipper/starter/app.js
+++
@@ -1,9 +0,0 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-
-const color = document.getElementById("color")
-const btn = document.getElementById("btn")
-
-btn.addEventListener('click',function(){
-    var random = Math.floor(Math.random() * colors.length);
-    document.body.style.backgroundColor = colors[random];
-});
