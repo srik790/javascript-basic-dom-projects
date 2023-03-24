957aa1d46972b343b2ee4d8399995f15dfcfdfd2
Shrikant Kumar <srik790@gmail.com>
2023-03-24 13:17:44 +0530

    new challenges added

---
+++ fix-read-more/app.js
@@ -0,0 +1,13 @@
+// ****** select items **********
+const btnRead = document.getElementById("btn1Read");
+const paragraph = document.getElementById(paragraph);
+// ****** event listeners **********
+// submit form
+btn.Read.addEventListener("click", showParagraph);
+
+// ****** functions **********
+
+function handleClick(){
+  paragraph.style.displayStyle = 'block';
+  btnRead.style.display = 'none';
+}
---
+++ hit-the-debugger/app.js
@@ -0,0 +1,10 @@
+// ****** select items **********
+const btnRead = document.getElementById("btnRead");
+// ****** event listeners **********
+btnRead.addEventListener("click", handleClick);
+
+// ****** functions **********
+
+function handleClick(){
+  debugger;
+}
